220898ba2613162d214bee315ec0fdb2b7cee39e
Elad Cohen <eladcohen25@MacBook-Pro-2.local>
2025-11-30 20:08:05 -0800

    Update favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/favicon.png" type="image/png" />
         <link rel="stylesheet" href="https://use.typekit.net/kxg5xem.css" />
       </head>
       <body>
